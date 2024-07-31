0c5aba8d8b9e3031186a173fc2f17c693d64d972
Bedrockbreaker <coolmandude101@gmail.com>
2024-07-31 01:20:39 -0700

    Remove uncessary css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import "./App.css";
-
 export function App() {
 
-	return <>
-		<h1 className="text-5xl">
+	return <div className="flex flex-col justify-center items-center w-screen h-screen dark:bg-neutral-800">
+		<h1 className="text-5xl dark:text-white">
 			Jenson Searle
 		</h1>
-		<p className="text-base">
+		<p className="text-base dark:text-white">
 			Gameplay & Systems Programmer, Game Designer, Fullstack Developer
 		</p>
-	</>;
+	</div>;
 }
