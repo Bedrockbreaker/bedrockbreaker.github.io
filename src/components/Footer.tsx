b183eefc0ddb7ec8a809d8be3f8d62aad0662b46
Bedrockbreaker <coolmandude101@gmail.com>
2024-08-03 00:37:32 -0700

    Remove period between copyright year and name

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -52,7 +52,7 @@ export function Footer() {
 				</div>
 				<div className="flex flex-row justify-between">
 					<p>
-						&copy; {new Date().getFullYear()}. Bedrockbreaker.
+						&copy; {new Date().getFullYear()} Bedrockbreaker.
 						Licensed under{" "}
 						<Link
 							to="https://github.com/Bedrockbreaker/bedrockbreaker.github.io/blob/main/LICENSE"
