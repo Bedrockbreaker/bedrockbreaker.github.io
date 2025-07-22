7cdceafaf6222a3bd44680a7005baa127a832d3e
Bedrockbreaker <coolmandude101@gmail.com>
2025-07-22 13:19:27 -0700

    Add stylized Ordered List component

--- src/Components/List.tsx
+++ src/Components/List.tsx
@@ -2,6 +2,10 @@ export function UL({className = "", ...props}: React.HTMLAttributes<HTMLUListEle
 	return <ul className={"my-6 ml-6 list-disc [&>li]:mt-2 " + className} {...props}/>;
 }
 
+export function OL({className = "", ...props}: React.HTMLAttributes<HTMLOListElement>) {
+	return <ol className={"my-6 ml-6 list-decimal [&>li]:mt-2 " + className} {...props}/>;
+}
+
 export function LI({className = "", ...props}: React.HTMLAttributes<HTMLLIElement>) {
 	return <li className={"leading-7 " + className} {...props}/>
 }
