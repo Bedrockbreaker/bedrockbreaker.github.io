e8055f5f57dae2c21a0e7df39166426d15f39c20
Bedrockbreaker <coolmandude101@gmail.com>
2025-09-12 23:16:52 -0700

    Add SIP Innovation "Brief"

--- src/Pages/SIP.tsx
+++ src/Pages/SIP.tsx
@@ -1,27 +1,19 @@
-import { Trans } from "react-i18next";
+import { useTranslation } from "react-i18next";
 
-import { Divider } from "~/Components/Divider";
-import { HoverTilt } from "~/Components/HoverTilt";
-import { LI, UL } from "~/Components/List";
-import { Text } from "~/Components/Text";
-import { Projects } from "~/Util/ProjectData";
+import { MDXComponents } from "~/Components/MDXComponents";
+import { MDXRenderer } from "~/Components/MDXRenderer";
+
+import SIP_en_US from "~/Content/en-US/Projects/SIP.mdx";
+import SIP_ja_JP from "~/Content/ja-JP/Projects/SIP.mdx";
 
 export function SIP() {
+	const { i18n } = useTranslation();
+
+	const Page = i18n.language === "ja-JP" ? SIP_ja_JP : SIP_en_US;
+
 	return <div className="flex flex-col p-6 max-w-7xl mx-auto grow overflow-x-hidden">
-		<Text variant="h1" className="mx-auto text-center">
-			<Trans i18nKey="sip.header">
-				text <span className="italic">text</span>
-			</Trans>
-		</Text>
-		<Text variant="lead" className="mx-auto text-center" t="sip.subtitle"/>
-		<Divider/>
-		<HoverTilt className="max-w-xl mx-auto">{Projects.memento.GetImage()}</HoverTilt>
-		<Text variant="h3" className="-mb-6" t="sip.overview.header"/>
-		<Text t="sip.overview.description"/>
-		<Text variant="h3" className="-mb-6" t="sip.innovation.header"/>
-		<UL>
-			<LI><Text t="sip.innovation.description.0"/></LI>
-			<LI><Text t="sip.innovation.description.1"/></LI>
-		</UL>
+		<MDXRenderer>
+			<Page components={MDXComponents}/>
+		</MDXRenderer>
 	</div>;
 }
